fa36276d5f015e6ee1a132b08b9e2a991e322727
Aleksandar Ilisić <149431814+alekssaa@users.noreply.github.com>
2024-12-12 18:31:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BackgroundBeamsDemo } from "./components/BackgroundBeamsDemo";
+import { VortexDemoSecond } from "./components/VortexDemoSecond";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { links } from "./data";
+import Error from "./components/Error";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          {links.map((link) => {
+            return (
+              <Route key={link.id} path={link.url} element={link.component} />
+            );
+          })}
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
